21f3cd5171f751e668cfc453ad9cc3932cbb6cae
Matt3ol <148490319+Matt3ol@users.noreply.github.com>
2024-01-25 09:26:42 +0100

    svolto

---
+++ es-2/src/assets/Hello.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Message from "./Message";
+
+const Hello = () => {
+  return (
+    <div>
+      <h2>Hello, World!</h2>
+      <Message />
+    </div>
+  );
+};
+
+export default Hello;
