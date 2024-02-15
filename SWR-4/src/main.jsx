d09d05376f52146ab0d48de9c801377af9847ead
Matt3ol <148490319+Matt3ol@users.noreply.github.com>
2024-02-15 12:34:08 +0100

    svolto

---
+++ SWR-4/src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { SWRConfig } from "swr";
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    
+  <SWRConfig value={{fetcher}}> 
+    <App />
+  </SWRConfig>
+);
