eb1009ba68a983e824988b763332635992dc8eaa
Matt3ol <148490319+Matt3ol@users.noreply.github.com>
2024-02-15 12:04:25 +0100

    si

---
+++ SWR/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import GitHubUser from "./GitHubUser."
+
+function App() {
+  const username = "matteo";
+
+  return (
+    <>
+  <GitHubUser username={username}></GitHubUser>
+    </>
+  );
+}
+
+export default App;
