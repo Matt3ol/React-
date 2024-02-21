a6b9899363436a8a36957eb258d13b50e20182c0
Matt3ol <148490319+Matt3ol@users.noreply.github.com>
2024-02-21 12:16:38 +0100

    si

--- unc-form/src/App.jsx
+++ unc-form/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { ControlledLogin } from './UncontrolledLogin'
+import { UncontrolledLogin } from './UncontrolledLogin'
 
 function App() {
 
 
   return (
     <>
-    <ControlledLogin></ControlledLogin>
+  <UncontrolledLogin></UncontrolledLogin>
 
     </>
   )
--- unc-form/src/UncontrolledLogin.jsx
+++ unc-form/src/UncontrolledLogin.jsx
@@ -1,24 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-export const ControlledLogin = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+export  function UncontrolledLogin () {
+   const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const username = form.elements.username.value;
+        const password = form.elements.password.value;
+        console.log('Username:', username);
+        console.log('Password:', password);
+      }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('Username:', username);
-    console.log('Password:', password);
-  };
+      return (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input type="text" name="username" id="username" />
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password" id="password" />
+          <button type="submit">Login</button>
+        </form>
+      );
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="username">Username:</label>
-      <input type="text" name="username" value={username} onChange={(event) => setUsername(event.target.value)} />
-      <br/>
-      <label htmlFor="password">Password:</label>
-      <input type="password" name="password" value={password} onChange={(event) => setPassword(event.target.value)} />
-      <br/>
-      <button type="submit">Login</button>
-    </form>
-  );
-};
+}
+
+
+
